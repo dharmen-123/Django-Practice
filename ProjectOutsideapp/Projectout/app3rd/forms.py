8e584f515c580bfce10192e07740de3d588036a4
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-14 15:36:09 +0530

    changes

---
+++ ProjectOutsideapp/Projectout/app3rd/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class StudentForm(forms.form):
+    name= forms.CharField()
+    email = forms.EmailField()
+    contact = forms.IntegerField()
+    image = forms.ImageField()
+    resume = forms.FileField()
