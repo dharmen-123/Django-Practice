529d0bf251a8583d22284921201453f65038c99e
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-23 18:04:04 +0530

    commit

--- QueryRerturnMultipleObject/ProjectQuery/app6th/views.py
+++ QueryRerturnMultipleObject/ProjectQuery/app6th/views.py
@@ -7,4 +7,9 @@ from .models import Student as Stu
 def home(self):
     all_data=Stu.objects.all()
     print(all_data)
+    for i in all_data:
+        print(i.name)
+        print(i.email)
+        print(i.contact)
+        print(i.city)
     print(all_data.values_list())
