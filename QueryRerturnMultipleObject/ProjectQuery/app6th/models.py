124c5c6861ba3693ad0463680d5c74938c10b0b2
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-23 17:06:50 +0530

    commit

--- QueryRerturnMultipleObject/ProjectQuery/app6th/models.py
+++ QueryRerturnMultipleObject/ProjectQuery/app6th/models.py
@@ -6,4 +6,5 @@ class Student(models.Model):
      email= models.EmailField()
      contact= models.IntegerField()
      city= models.CharField(max_length=70)
-     
+     def __str__(self):
+        return self.name
