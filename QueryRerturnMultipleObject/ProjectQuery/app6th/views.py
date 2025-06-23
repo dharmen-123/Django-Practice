3fa36dd51eb0fcf1d232f608c6ef85e6261e5b73
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-23 17:58:53 +0530

    changes

--- QueryRerturnMultipleObject/ProjectQuery/app6th/views.py
+++ QueryRerturnMultipleObject/ProjectQuery/app6th/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Student as Stu
+
+def home(self):
+    all_data=Stu.objects.all()
+    print(all_data)
+    print(all_data.values_list())
