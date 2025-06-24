20e42e6c5e94ba6303dde00f7a9e3c8bdb634a71
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-24 18:27:44 +0530

    commit

--- QueryReturnSigleObj/ProjectSingleobj/app8th/views.py
+++ QueryReturnSigleObj/ProjectSingleobj/app8th/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import Student as Stu
 def home(req):
-     data=Stu.objects.get(id=2)
-     print(data)
-     print(data.name,data.email,data.city,data.contact)
-     return  HttpResponse(data)
+     # data=Stu.objects.get(id=2)
+     # print(data)
+     # print(data.name,data.email,data.city,data.contact)
+
+     # data2=Stu.objects.first()
+     # data3=Stu.objects.last()
+
+     data4=Stu.objects.order_by('name').first()
+     # data4=Stu.objects.order_by('name').last()
+     return  HttpResponse(data4)
