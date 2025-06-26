b3f40aa69acdd79bc2f250f671d11ee206777818
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-26 17:37:37 +0530

    changes

--- ModelInharitance/Projectmodels/app9th/models.py
+++ ModelInharitance/Projectmodels/app9th/models.py
@@ -2,24 +2,44 @@ from django.db import models
 
 # Create your models here.
 
-class BaseInfo(models.Model):
+# class BaseInfo(models.Model):
+#      name=models.CharField(max_length=50)
+#      email=models.EmailField()
+#      contact=models.IntegerField()
+#      class Meta:
+#         abstract = True
+
+# class Employee(BaseInfo):
+#      empid=models.IntegerField()
+#      class Meta:
+#            db_table='Employee'
+#            verbose_name_plural='Employee'
+
+
+# class Client(BaseInfo):
+#      userid=models.IntegerField()
+#      prject=models.CharField()
+#      class Meta:
+#            db_table='Client'
+#            verbose_name_plural='Client'
+           
+#
+class Basedata(models.Model):
      name=models.CharField(max_length=50)
      email=models.EmailField()
      contact=models.IntegerField()
-     class Meta:
-        abstract = True
+     def __str__(self):
+        return self.name
 
-class Employee(BaseInfo):
+class Employe(Basedata):
      empid=models.IntegerField()
-     class Meta:
-           db_table='Employee'
-           verbose_name_plural='Employee'
-
-
-class Client(BaseInfo):
-     userid=models.IntegerField()
-     prject=models.CharField()
-     class Meta:
-           db_table='Client'
-           verbose_name_plural='Client'
-           
+     salary=models.IntegerField()
+     def __str__(self):
+        return self.name
+        
+class Student(Basedata):
+     Branch=models.CharField()
+     fees=models.IntegerField()
+     def __str__(self):
+        return self.name
+                   
