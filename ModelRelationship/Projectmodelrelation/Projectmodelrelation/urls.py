106b60cebe433977215027e46f033d7966374641
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-27 23:04:52 +0530

    commit

--- ModelRelationship/Projectmodelrelation/Projectmodelrelation/urls.py
+++ ModelRelationship/Projectmodelrelation/Projectmodelrelation/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app10th import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.aadhar,name='aadhar'),
+    path('',views.Student,name='Student'),
 ]
--- ModelRelationship/Projectmodelrelation/app10th/views.py
+++ ModelRelationship/Projectmodelrelation/app10th/views.py
@@ -1,4 +1,48 @@
 from django.shortcuts import render
-
 # Create your views here.
 from .models import Addhar,Student
+
+
+# Create your views here.
+
+def student(request):
+    data = Student.objects.all()
+    print(data.values())
+    print("forword access-----------------")
+    student = Student.objects.get(name = "Rahul")
+    print(student)
+    stu_to_aadhar = student.aadhar_no
+    print(stu_to_aadhar.aadhar)
+    print(stu_to_aadhar.createdate)
+    print(stu_to_aadhar.createdby)
+    # my_list = []
+    # for i in all_data:
+    #     name = i.stu_name
+    #     email = i.stu_email
+    #     contact = i.stu_contact
+    #     aadhar_no = i.aadhar_no
+    #     created_date = i.aadhar_no.create_date
+    #     created_by = i.aadhar_no.created_by
+    #     data = {
+    #         'name':name,
+    #         'email':email,
+    #         'contact':contact,
+    #         'aadhar_no':aadhar_no,
+    #         'created_date':created_date,
+    #         'created_by':created_by
+    #     }
+    #     my_list.append(data)
+    # print(my_list)
+    
+def aadhar(request):
+    all_data = Aadhar.objects.all()
+    print(all_data.values())
+    print(all_data.values_list())
+    print("reverse access------------------------- related_name='student_info'") 
+    aadhar = Aadhar.objects.get(aadhar='12345')
+    print(aadhar)
+    print(aadhar.student_info)
+    print(aadhar.student_info.stu_name)
+    print(aadhar.student_info.stu_email)
+    print(aadhar.student_info.contact)
+    print(aadhar.student_info.aadhar_no)
