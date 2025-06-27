8fbb39a63eebf98211f76e59a84d4782a7ea2630
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-27 18:09:16 +0530

    commit

--- ModelRelationship/Projectmodelrelation/app10th/models.py
+++ ModelRelationship/Projectmodelrelation/app10th/models.py
@@ -2,3 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Addhar(modes.Model):
+    aadhar=models.IntegerField(unique=True)
+    createdate=models.DateTimeField()
+    createby=models.CharField(max_length=20)
+    def __str__(self):
+        return str(self.aadhar)
+
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    contact=models.IntegerField()
+    addhar_no=models.OneToOneField(Addhar,on_delete=models.PROTECT,to_field='aadhar',related_name='student')
+    def __str__(self):
+        return self.name
