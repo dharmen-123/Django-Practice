eb3de01ffbe7dfc0847073c7dcac30901eb6f9ae
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-19 16:36:44 +0530

    commit

--- ADDTOCART/ProjectCart/appcart/views.py
+++ ADDTOCART/ProjectCart/appcart/views.py
@@ -182,9 +182,9 @@ def paymenthandler(request):
 
             send_mail(
                 "Payment done", 
-                "Elctronic Item Payment done Successfully.",
-                "premv6264@gmail.com",
-                ["dharmendrachilhate11@gmail.com"],
+                "A successful payment is a transaction where a customer's payment method is successfully processed, resulting in the completion of a purchase or service. This means the customer's funds have been transferred to the recipient, and the transaction is finalized without any errors or issues. It's a critical indicator of a smooth checkout experience and directly impacts customer satisfaction and business revenue. ",
+                "dharmendrachilhate11@gmail.com",
+                ["premv6264@gmail.com","viplovesana90@gmail.com","pathakaditya1418@gmail.com"],
                 fail_silently=False,
             )
             return redirect('home')
