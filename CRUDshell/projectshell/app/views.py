bb2dfbe494fd08298afdf243d199d863ea40b55e
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-24 22:25:57 +0530

    commit

--- CRUDshell/projectshell/app/views.py
+++ CRUDshell/projectshell/app/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
+from .models import Student
 
 # Create your views here.
 
-from .models import Student
-
 def home(request):
     return HttpResponse("Hello python ..................")
-    
