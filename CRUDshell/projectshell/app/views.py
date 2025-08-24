75b4e9fbabbb642e08e8dbd96ec9a2bf5a8b0fdc
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-24 23:02:55 +0530

    commit

--- CRUDshell/projectshell/app/views.py
+++ CRUDshell/projectshell/app/views.py
@@ -6,3 +6,29 @@ from .models import Student
 
 def home(request):
     return HttpResponse("Hello python ..................")
+
+
+### CRUD Operation on shell ########
+
+##<-------- Create ------------------>
+# Student.objects.create(name="name",email="email",........)
+##  or
+# a=Student(name="name",email="email",............)
+# a.save()
+
+## <------------ Read ----------->
+# data=Student.objects.all()
+# print(data)
+#    OR
+# Student.objects.values()
+
+## <----------- Update ------------>
+#  a=Student.objects.get(id=2)
+# a.name=newname
+#a.save()
+
+
+
+
+
+
