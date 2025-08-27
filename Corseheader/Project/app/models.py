3ea11aea65f389011cb487d131caff4b5a1b11c3
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-27 22:21:04 +0530

    commit

--- Corseheader/Project/app/models.py
+++ Corseheader/Project/app/models.py
@@ -3,4 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    pass
+    firstname=models.CharField(max_length=150)
+    lastname=models.CharField(max_length=150)
+    email=models.EmailField()
+    password=models.CharField()
+
+    class Meta:
+        db_table='Student'
