48eb31018a35d38ebdcf9ea67234d350f5345f8d
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-09-07 21:42:35 +0530

    otp commit

--- Otpvalidation/Projectotp/appotp/views.py
+++ Otpvalidation/Projectotp/appotp/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render,redirect
 from django.core.mail import send_mail
-
+import random
 # Create your views here.
 
 def home(request):
     return  render(request,'home.html')
 
+def generate(request):
+    return str(random.randint(100000,999999))
+
 def sendotp(request):
     if request.method=='POST':
-        
+        email=request.POST.get('email')
+        otp=generate()
         send_mail(
             "Payment done", 
-            "A successful payment is a transaction where a customer's payment method is successfully processed, resulting in the completion of a purchase or service. This means the customer's funds have been transferred to the recipient, and the transaction is finalized without any errors or issues. It's a critical indicator of a smooth checkout experience and directly impacts customer satisfaction and business revenue. ",
+            # "A successful payment is a transaction where a customer's payment method is successfully processed, resulting in the completion of a purchase or service. This means the customer's funds have been transferred to the recipient, and the transaction is finalized without any errors or issues. It's a critical indicator of a smooth checkout experience and directly impacts customer satisfaction and business revenue. ",
+            f"This is your Register otp {otp} ",
             "dharmendrachilhate11@gmail.com",
-            [],
+            [email],
             fail_silently=False,
         )
-        return redirect('home')
+        return render(request,'home.html',{'email':email})
 
 
 
