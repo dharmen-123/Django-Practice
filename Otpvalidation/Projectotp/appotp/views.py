a77cfdf07e268ec17069014cbee704d5dc451913
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-09-07 21:28:11 +0530

    otp validation

--- Otpvalidation/Projectotp/appotp/views.py
+++ Otpvalidation/Projectotp/appotp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+
+def home(request):
+    return  render(request,'home.html')
+
+def sendotp(request):
+    return redirect('home')
+
+
+
