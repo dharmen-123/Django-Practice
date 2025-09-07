278cd4c035b698017d871b1aed45b8994ddd9baa
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-09-07 21:35:47 +0530

    commit

--- Otpvalidation/Projectotp/appotp/views.py
+++ Otpvalidation/Projectotp/appotp/views.py
@@ -7,15 +7,16 @@ def home(request):
     return  render(request,'home.html')
 
 def sendotp(request):
-
-    send_mail(
-                "Payment done", 
-                "A successful payment is a transaction where a customer's payment method is successfully processed, resulting in the completion of a purchase or service. This means the customer's funds have been transferred to the recipient, and the transaction is finalized without any errors or issues. It's a critical indicator of a smooth checkout experience and directly impacts customer satisfaction and business revenue. ",
-                "dharmendrachilhate11@gmail.com",
-                ["harsh14thakre@gmail.com"],
-                fail_silently=False,
-    )
-    return redirect('home')
+    if request.method=='POST':
+        
+        send_mail(
+            "Payment done", 
+            "A successful payment is a transaction where a customer's payment method is successfully processed, resulting in the completion of a purchase or service. This means the customer's funds have been transferred to the recipient, and the transaction is finalized without any errors or issues. It's a critical indicator of a smooth checkout experience and directly impacts customer satisfaction and business revenue. ",
+            "dharmendrachilhate11@gmail.com",
+            [],
+            fail_silently=False,
+        )
+        return redirect('home')
 
 
 
